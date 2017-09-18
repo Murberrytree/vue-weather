bf7ab3422afcdc4df6bd9ee04838425fb578fcb0
Murberrytree <k23sh138@gmail.com>
2017-09-18 22:32:27 +0900

    vue-weather inital

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <div id="information">
+      <div id="current-weather">
+        <current-weather />
+      </div>
+      <hr>
+      <div id="forecast">
+        <forecast></forecast>
+        <hr>
+      </div>
+    </div>
+    <div id="map">
+      <google-map></google-map>
+    </div>
+  </div>
+</template>
+
+<script>
+  import CurrentWeather from './currentWeather.vue'
+  import GoogleMap from './components/googleMap.vue'
+  import Forecast from './components/ForeCast.vue'
+  export default {
+    name: 'app',
+    components: {
+      CurrentWeather,
+      GoogleMap,
+      Forecast
+    }
+  }
+</script>
+
+<style lang="scss">
+$point-color:#fe3f32;
+body{
+  background-color:#fafafa;
+}
+  #app{
+    position:absolute;
+    left:50%;
+    top:50%;
+    width:1000px;
+    height:600px;
+    margin-top:-300px;
+    margin-left:-500px;
+    border-radius:3px;
+    background-color:#444;
+    box-shadow:0px 2px 2px 2px rgba(0,0,0,0.3);
+    &:after{
+      content:"";
+      display: block;
+      clear:both;
+    }
+    #information{
+      float:left;
+      width:600px;
+      #current-weather{
+        height:180px;
+        padding-top:20px;
+      }
+      #forecast{
+        height:400px;
+      }
+    }
+    #map{
+      float:left;
+      width:400px;
+      height:600px;
+    }
+  }
+  hr{
+    width:80%;
+    margin:80px auto;
+    border:0;
+    border-top:1px solid #999;
+  }
+</style>
---
+++ src/currentWeather.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="location">{{ location }}</div>
+    <div class="weather">{{ weather }}</div>
+    <div class="temp">{{ temp }}&#176;C</div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'current-weather',
+    data () {
+      return {
+        location: 'Seoul',
+        weather: 'Rain',
+        temp: '30c',
+        lat: 37.566535,
+        lon: 126.977969
+      }
+    },
+    methods: {
+      setCoordinate (coordinate) {
+        this.lat = coordinate.lat
+        this.lon = coordinate.lng
+
+        this.axios.get('https://api.openweathermap.org/data/2.5/weather', {
+          params: {
+            lat: this.lat,
+            lon: this.lon,
+            APPID: 'c5d698f455ddeab167e90c6f9a276f3e'
+          }
+        })
+          .then(response => {
+            let data = response.data
+            this.location = data.name
+            this.weather = data.weather[0].main
+            this.temp = (data.main.temp - 273.15).toFixed(0)
+          })
+      }
+    },
+    mounted () {
+      this.$bus.$on('sendCoordinate', this.setCoordinate)
+    }
+  }
+</script>
+
+<style scoped>
+  .location{
+    text-align:center;
+    font-size:30px;
+    color:#fff;
+    padding-top:20px;
+  }
+  .weather{
+    text-align: center;
+    font-size:20px;
+    color:#fff;
+  }
+  .temp{
+    text-align:center;
+    font-size:50px;
+    color:#fff;
+  }
+</style>
